82b4dc762dcb479b7463654b44362408d06ff1a3
Eric Scouten <git@scouten.me>
2024-04-27 22:33:22 -0700

    Update IIW38 article with processed photos

---
+++ content/2024/iiw38/markers.js
@@ -0,0 +1,14 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4640-061", 37.414000, -122.077667),
+        addGpxMarker(map, "es-4640-063", 37.414000, -122.077500),
+        addGpxMarker(map, "es-4640-077", 37.414667, -122.077000),
+        addGpxMarker(map, "es-4640-084", 37.414333, -122.077667),
+        addGpxMarker(map, "jw-244-a379", 37.414333, -122.077667),
+        addGpxMarker(map, "es-4641-002", 37.414500, -122.077500),
+        addGpxMarker(map, "jf-244-2470", 37.414500, -122.077667),
+        addGpxMarker(map, "jf-244-2473", 37.414500, -122.077667),
+        addGpxMarker(map, "es-4641-003", 37.414500, -122.077667),
+        addGpxMarker(map, "es-4641-004", 37.414500, -122.077667),
+    ]
+}
