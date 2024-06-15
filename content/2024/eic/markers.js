5b6a47ef124568f4f57f6459a34f10732b4e499f
Eric Scouten <git@scouten.me>
2024-06-14 19:26:30 -0700

    New photos from Tuesday

--- content/2024/eic/markers.js
+++ content/2024/eic/markers.js
@@ -1,5 +1,7 @@
 function addGpxMarkers(map) {
     return [
         addGpxMarker(map, "es-4661-096", 52.521000, 13.416833),
+        addGpxMarker(map, "es-4661-110", 52.520667, 13.415667),
+        addGpxMarker(map, "es-4662-004", 52.520717, 13.416470),
     ]
 }
