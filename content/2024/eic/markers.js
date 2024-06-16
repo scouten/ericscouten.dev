b57884080740b6c18abafc9b5464b2f4f00a1e3f
Eric Scouten <git@scouten.me>
2024-06-15 20:26:28 -0700

    EIC Friday sessions and wrap up

--- content/2024/eic/markers.js
+++ content/2024/eic/markers.js
@@ -3,5 +3,6 @@ function addGpxMarkers(map) {
         addGpxMarker(map, "es-4661-096", 52.521000, 13.416833),
         addGpxMarker(map, "es-4661-110", 52.520667, 13.415667),
         addGpxMarker(map, "es-4662-004", 52.520717, 13.416470),
+        addGpxMarker(map, "jf-246-2746", 52.520667, 13.416000),
     ]
 }
